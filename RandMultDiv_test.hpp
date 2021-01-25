79e32154c3e9ca2e339d3aa28bef557027f943e8
rddlamas@gmail.com <rlama005@hammer.cs.ucr.edu>
2021-01-25 14:55:07 -0800

    Creating the file which will containt all the unit test for classes

---
+++ RandMultDiv_test.hpp
@@ -0,0 +1,12 @@
+#ifndef __RAND_MULT_DIV_TEST_HPP__
+#define __RAND_MULT_DIV_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "base.hpp"
+#include "op.hpp"
+
+
+
+
+#endif//__RAND_MULT_DIV_TEST_HPP__
