ec1a476fa15797079fe37d7068fe77470b787d91
rddlamas@gmail.com <rlama005@hammer.cs.ucr.edu>
2021-01-26 02:06:06 -0800

    Added test cases for decimal divition that outputed infinite decinaml places

--- RandMultDiv_test.hpp
+++ RandMultDiv_test.hpp
@@ -72,6 +72,20 @@ TEST(DivTest, DivEvalNeg){
 }
 
 TEST(DivTest, DivEvalDecimal){
+	Op* three = new Op(3);
+	Op* two = new Op(2);
+	Div* test = new Div(three, two);
+	EXPECT_EQ(test->evaluate(), 1.5);
+	EXPECT_EQ(test->stringify(), "3.000000 / 2.000000");
+}
+TEST(DivTest, DivEvalInfiniteDecimals){
+	Op* three = new Op(3);
+	Op* one = new Op(1);
+	Div* test = new Div(one, three);
+	EXPECT_EQ(test->stringify(), "1.000000 / 3.000000" );
+	EXPECT_NEAR(test->evaluate(),0.333333, 0.333 );
+}
+TEST(DivTest, DivEvalZero){
 	Op* one = new Op(1);
 	Op* zero = new Op(0);
 	Div* test = new Div(one, zero);
